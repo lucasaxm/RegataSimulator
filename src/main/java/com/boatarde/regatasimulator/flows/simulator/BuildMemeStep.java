ffe31ebc69fd833c705bf25acce0afdf5fe2b0fd
Lucas Morais <lucas.axm@gmail.com>
2024-09-26 16:15:09 -0300

    feat: MAGICK_PATH env variable

--- src/main/java/com/boatarde/regatasimulator/flows/simulator/BuildMemeStep.java
+++ src/main/java/com/boatarde/regatasimulator/flows/simulator/BuildMemeStep.java
@@ -7,6 +7,7 @@ import com.boatarde.regatasimulator.flows.WorkflowStep;
 import com.boatarde.regatasimulator.flows.WorkflowStepRegistration;
 import com.boatarde.regatasimulator.models.TemplateArea;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -17,6 +18,12 @@ import java.util.List;
 @Slf4j
 @WorkflowStepRegistration(WorkflowAction.BUILD_MEME_STEP)
 public class BuildMemeStep implements WorkflowStep {
+
+    private final String magickPath;
+
+    public BuildMemeStep(@Value("${magick.path}") String magickPath) {
+        this.magickPath = magickPath;
+    }
 
     @Override
     public WorkflowAction run(WorkflowDataBag bag) {
@@ -40,8 +47,8 @@ public class BuildMemeStep implements WorkflowStep {
     }
 
     private Path buildMeme(Path templateFile, Path sourceFile, TemplateArea templateArea) throws Exception {
-        log.info("running command: magick identify -format %w %h {}", templateFile.toString());
-        ProcessBuilder pb = new ProcessBuilder("magick", "identify", "-format", "%w %h", templateFile.toString());
+        log.info("running command: {} identify -format %w %h {}", magickPath, templateFile.toString());
+        ProcessBuilder pb = new ProcessBuilder(magickPath, "identify", "-format", "%w %h", templateFile.toString());
         Process process = pb.start();
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String[] dimensions = reader.readLine().split(" ");
@@ -51,9 +58,9 @@ public class BuildMemeStep implements WorkflowStep {
 
         // Resize source image
         Path resizedSource = Paths.get("resized_source.png");
-        log.info("running command: magick {} -resize {}x{}! {}",
-            sourceFile.toString(), width, height, resizedSource);
-        pb = new ProcessBuilder("magick", sourceFile.toString(), "-resize", width + "x" + height + "!",
+        log.info("running command: {} {} -resize {}x{}! {}", magickPath, sourceFile.toString(), width, height,
+            resizedSource);
+        pb = new ProcessBuilder(magickPath, sourceFile.toString(), "-resize", width + "x" + height + "!",
             resizedSource.toString());
         pb.start().waitFor();
 
@@ -64,9 +71,9 @@ public class BuildMemeStep implements WorkflowStep {
             height, templateArea.getBottomLeft().getX(), templateArea.getBottomLeft().getY(),
             width, templateArea.getTopRight().getX(), templateArea.getTopRight().getY(),
             width, height, templateArea.getBottomRight().getX(), templateArea.getBottomRight().getY());
-        log.info("running command: magick {} -alpha set -virtual-pixel transparent -distort Perspective {} {}",
-            resizedSource, coordinates, distortedSourceTemp);
-        pb = new ProcessBuilder("magick", resizedSource.toString(), "-alpha", "set", "-virtual-pixel", "transparent",
+        log.info("running command: {} {} -alpha set -virtual-pixel transparent -distort Perspective {} {}",
+            magickPath, resizedSource, coordinates, distortedSourceTemp);
+        pb = new ProcessBuilder(magickPath, resizedSource.toString(), "-alpha", "set", "-virtual-pixel", "transparent",
             "-distort", "Perspective", coordinates, distortedSourceTemp.toString());
         pb.start().waitFor();
 
@@ -77,25 +84,25 @@ public class BuildMemeStep implements WorkflowStep {
             templateArea.getTopRight().getX(), templateArea.getTopRight().getY(),
             templateArea.getBottomRight().getX(), templateArea.getBottomRight().getY(),
             templateArea.getBottomLeft().getX(), templateArea.getBottomLeft().getY());
-        log.info("running command: magick -size {}x{} xc:black -fill white -draw {} {}", width, height, drawCommand,
-            mask);
-        pb = new ProcessBuilder("magick", "-size", width + "x" + height, "xc:black", "-fill", "white",
+        log.info("running command: {} -size {}x{} xc:black -fill white -draw {} {}", magickPath, width, height,
+            drawCommand, mask);
+        pb = new ProcessBuilder(magickPath, "-size", width + "x" + height, "xc:black", "-fill", "white",
             "-draw", drawCommand, mask.toString());
         pb.start().waitFor();
 
         // Apply mask to distorted source
         Path distortedSource = Paths.get("distorted_source.png");
-        log.info("running command: magick {} {} -alpha off -compose CopyOpacity -composite {}",
+        log.info("running command: {} {} {} -alpha off -compose CopyOpacity -composite {}", magickPath,
             distortedSourceTemp, mask, distortedSource);
-        pb = new ProcessBuilder("magick", distortedSourceTemp.toString(), mask.toString(), "-alpha", "off",
+        pb = new ProcessBuilder(magickPath, distortedSourceTemp.toString(), mask.toString(), "-alpha", "off",
             "-compose", "CopyOpacity", "-composite", distortedSource.toString());
         pb.start().waitFor();
 
         // Composite final image
         Path finalOutput = Paths.get("final_output.png");
-        log.info("running command: magick composite -compose Over {} {} {}", distortedSource, templateFile,
+        log.info("running command: {} composite -compose Over {} {} {}", magickPath, distortedSource, templateFile,
             finalOutput);
-        pb = new ProcessBuilder("magick", "composite", "-compose", "Over", distortedSource.toString(),
+        pb = new ProcessBuilder(magickPath, "composite", "-compose", "Over", distortedSource.toString(),
             templateFile.toString(), finalOutput.toString());
         pb.start().waitFor();
 
