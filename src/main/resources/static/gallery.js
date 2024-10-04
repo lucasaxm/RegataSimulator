3b9ef757f77a5fedf4d18a184e9726be79ef5069
Lucas Morais <lucas.axm@gmail.com>
2024-10-04 19:17:10 -0300

    feat: Templates Review tab

--- src/main/resources/static/gallery.js
+++ src/main/resources/static/gallery.js
@@ -15,6 +15,14 @@ function galleryApp() {
         darkMode: false,
         ITEMS_PER_PAGE: 12,
 
+        templateReviewItems: [],
+        templateReviewCurrentPage: 1,
+        templateReviewTotalItems: 0,
+        templateReviewTotalPages: 1,
+        reviewApproved: false,
+        reviewRefused: false,
+        reviewReason: '',
+
         init() {
             this.loadSources(this.sourceCurrentPage);
             this.darkMode = localStorage.getItem('darkMode') === 'true';
@@ -92,6 +100,68 @@ function galleryApp() {
 
         applyDarkMode() {
             document.documentElement.setAttribute('data-bs-theme', this.darkMode ? 'dark' : 'light');
+        },
+
+        async loadTemplateReview(page) {
+            this.templateReviewCurrentPage = page;
+            const response = await fetch(`/api/templates/review?page=${page}&perPage=${this.ITEMS_PER_PAGE}`);
+            const data = await response.json();
+            this.templateReviewItems = data.items;
+            this.templateReviewTotalItems = data.totalItems;
+            this.templateReviewTotalPages = Math.ceil(this.templateReviewTotalItems / this.ITEMS_PER_PAGE);
+        },
+
+        changeTab(tab) {
+            this.currentTab = tab;
+            if (tab === 'sources') {
+                this.loadSources(this.sourceCurrentPage);
+            } else if (tab === 'templates') {
+                this.loadTemplates(this.templateCurrentPage);
+            } else if (tab === 'templateReview') {
+                this.loadTemplateReview(this.templateReviewCurrentPage);
+            }
+        },
+
+        async openTemplateReviewModal(item) {
+            this.selectedTemplate = item;
+            this.modalImage = `/api/templates/review/${item.id}`;
+            this.reviewApproved = false;
+            this.reviewRefused = false;
+            this.reviewReason = '';
+            const modal = new bootstrap.Modal(this.$refs.imageModal);
+            modal.show();
+        },
+
+        async sendReview() {
+            if (!this.reviewApproved && !this.reviewRefused) {
+                alert('Please select either Approve or Refuse.');
+                return;
+            }
+
+            if (this.reviewRefused && !this.reviewReason) {
+                alert('Please provide a reason for refusing the template.');
+                return;
+            }
+
+            const reviewData = {
+                approved: this.reviewApproved,
+                reason: this.reviewRefused ? this.reviewReason : null
+            };
+
+            const response = await fetch(`/api/templates/review/${this.selectedTemplate.id}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(reviewData)
+            });
+
+            if (response.ok) {
+                bootstrap.Modal.getInstance(this.$refs.imageModal).hide();
+                this.loadTemplateReview(this.templateReviewCurrentPage);
+            } else {
+                alert('Failed to submit the review. Please try again.');
+            }
         }
     };
 }
