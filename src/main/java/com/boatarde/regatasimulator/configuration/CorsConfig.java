d61aa6a25b72855de29f172ac9d19a92999ea3e7
Lucas Morais <lucas.axm@gmail.com>
2026-01-07 01:29:40 -0300

    fix: update CORS allowed origins and session cookie domain (#22)

--- src/main/java/com/boatarde/regatasimulator/configuration/CorsConfig.java
+++ src/main/java/com/boatarde/regatasimulator/configuration/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://boatarde.dev.br")
+            .allowedOrigins("https://boatarde.dev.br", "https://boatarde.dev")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
